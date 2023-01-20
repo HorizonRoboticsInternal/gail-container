27548c4fa1b55e6d95e2f5922d30aa762c7fb5e1
Break Yang <breakds@gmail.com>
2023-01-20 14:15:06 -0800

    Add nix built docker

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "Containers for development of General AI Lab";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    utils.url = "github:numtide/flake-utils";
+
+    alf-devenv.url = "github:HorizonRobotics/alf-nix-devenv";
+    alf-devenv.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: inputs.utils.lib.eachSystem [
+    "x86_64-linux"
+  ] (system:
+    let pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [
+            inputs.alf-devenv.overlays.hobot
+          ];            
+        };
+    in {
+      packages = {
+        hobot-cicd = pkgs.dockerTools.buildImage {
+          name = "hobot-cicd";
+          tag = "latest";
+          
+          copyToRoot = pkgs.buildEnv {
+            name = "root-environment";
+            paths = with pkgs; [
+              bash
+              coreutils
+              git
+              (python3.withPackages (pyPkgs: with pyPkgs; [
+                alf-cpu
+
+                # Utils
+                numpy-quaternion
+
+                # Simulators
+                mujoco-pybind
+                dm-control
+
+                # Deveopment Tools
+                yapf
+                pylint
+                pre-commit
+              ]))
+            ];
+            pathsToLink = [ "/bin" ];
+          };
+
+          config.Cmd = [ "/bin/bash" ];
+        };
+      };
+    });
+}
